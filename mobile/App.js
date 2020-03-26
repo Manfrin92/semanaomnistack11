5b52bee8013547399207b84274579cc566b4c5bc
Manfrin92 <fk.manfrin@gmail.com>
2020-03-26 14:50:59 -0300

    Mobile da aplicacao

---
+++ mobile/App.js
@@ -0,0 +1,18 @@
+import "intl";
+import "intl/locale-data/jsonp/pr-BR";
+
+import React from 'react';
+
+
+import { StyleSheet, Text, View } from 'react-native';
+
+
+import Routes from "./src/routes";
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
